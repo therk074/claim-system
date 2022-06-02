3afb6e817d4b75274300efd354a48b84eb82962f
k <mju5906105378@mju.ac.th>
2022-06-02 11:45:42 +0700

    edit role path

--- src/users/schema/users.schema.ts
+++ src/users/schema/users.schema.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-import { Role } from 'src/role/role.enum';
+import { Role } from '../../role/role.enum';
 
 export type UserDocument = User & Document;
 
